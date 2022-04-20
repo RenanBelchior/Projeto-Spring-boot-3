ee640fb3f15962ff62d55bad6a55413f9ceecff2
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2022-04-20 02:32:38 -0300

    Add files via upload

---
+++ springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
@@ -0,0 +1,18 @@
+package com.digitalinnovationone.springbootconfig;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+
+@RestController
+public class AppController {
+
+	@Value("${app.message}")
+	private String appMessage;
+	
+	@GetMapping("/")
+	public String getAppMessage() {
+		return appMessage;
+	}
+}
---
+++ springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/config/DBconfiguration.java
@@ -0,0 +1,42 @@
+package com.digitalinnovationone.springbootconfig.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties("spring.source")
+@Getter
+@Setter
+public class DBconfiguration {
+
+	private String driverClassName;
+	private String url;
+	private String username;
+	private String password;
+	
+	
+	
+	@Profile("dev")
+	@Bean
+	public String testDataBaseConnection() {
+		System.out.println("DB connection for DEV - H2");
+		System.out.println(driverClassName);
+		System.out.println(url);
+		return "DB connection to H2_TEST - Test instance";
+	}
+	
+	@Profile("prod")
+	@Bean
+	public String productionDataBaseConnection() {
+		System.out.println("DB connection for Production - MySQL");
+		System.out.println(driverClassName);
+		System.out.println(url);
+		return "DB connection to MYSQL_PROD - Production instance";
+		
+	}
+}
